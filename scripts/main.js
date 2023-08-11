e006a4a77e38205af1e40736f1318b6573a0a349
PrakGov <55034062+prak112@users.noreply.github.com>
2023-08-11 18:00:02 +0300

    add on-click event to change image

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,25 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Introducing... The Super Cats!";
 
-// add click event to list elements
+// sample click event to list elements
 document.querySelector("ul").addEventListener("click", () => {
-    alert("..and You have touched Greatness! Cat-Lord blesses You!");
+    alert(`
+    You have touched (clicked) on Greatness! Cat-Lord blesses You!
+    
+                ~§~§~Your Quest~§~§~
+    Identify the Firefox among Us! (Click and See)`);
 });
+
+// on-click event to img element - change image
+const catImage = document.querySelector("img");
+catImage.onclick = () => {
+    const imgSrc = catImage.getAttribute("src");
+    if (imgSrc === "/images/cats/waltz-cat.jpg"){
+        catImage.setAttribute("src", "/images/Firefox_logo_2017.png");
+    }
+    else{
+        catImage.setAttribute("src", "/images/cats/waltz-cat.jpg");
+    }
+};
+
+
