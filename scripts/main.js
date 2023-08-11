3165d73f364c6e2cd60e94a9e07e597e3de879de
PrakGov <55034062+prak112@users.noreply.github.com>
2023-08-11 17:30:41 +0300

    comment code, add click event

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,9 @@
+// source html - cat_index.html
+// modify header
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Introducing... The Super Cats!";
+
+// add click event to list elements
+document.querySelector("ul").addEventListener("click", () => {
+    alert("..and You have touched Greatness! Cat-Lord blesses You!");
+});
