3c3bf586edc2aea03bb2fef246a96a46291e1369
PrakGov <55034062+prak112@users.noreply.github.com>
2023-08-11 18:57:17 +0300

    rename file, add button functionality

---
+++ scripts/cat_main.js
@@ -0,0 +1,53 @@
+// source html - cat_index.html
+// modify header
+const introHeading = document.querySelector("h1");
+introHeading.textContent = "Introducing... The Super Cats!";
+
+// sample click event to list elements
+document.querySelector("ul").addEventListener("click", () => {
+    alert(`
+    You have touched (clicked) on Greatness! Cat-Lord blesses You!
+    
+                ~§~§~|| Your Quest ||~§~§~
+    Identify the Firefox among Us! (Click each photo and See)`);
+});
+
+// on-click event to img element - change image
+const catImage = document.querySelector("img");
+catImage.onclick = () => {
+    const imgSrc = catImage.getAttribute("src");
+    if (imgSrc === "/images/cats/waltz-cat.jpg"){
+        catImage.setAttribute("src", "/images/Firefox_logo_2017.png");
+    }
+    else{
+        catImage.setAttribute("src", "/images/cats/waltz-cat.jpg");
+    }
+};
+
+// button functionality for personalised message
+let changeButton = document.querySelector("button");
+let introHeading = document.querySelector("h1");
+
+function setUserName(){
+    const userName = prompt("Enter thy name, Young Padawan!");
+    if(!userName){
+        setUserName();
+    }
+    else{
+        localStorage.setItem("name", userName);
+        introHeading.textContent = `Welcome to the Super Cats photo gallery, Young Padawan ${userName}`;
+    }    
+}
+
+// function initialization
+if(!localStorage.getItem("name")){
+    setUserName();
+}
+else{
+    const storedName = localStorage.getItem("name");
+    introHeading.textContent = `Welcome to the Super Cats photo gallery, Young Padawan ${storedName}`;
+}
+
+changeButton.onclick = () => { 
+    setUserName();
+};
